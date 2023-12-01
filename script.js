5fa670bdee54c740c71926f2d4779df8a9d93123
Saiyam Verma <121923836+Vermasaiyam@users.noreply.github.com>
2023-12-01 20:50:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let input = document.getElementById('input');
+let buttons = document.querySelectorAll("button");
+let string = "";
+
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        string = input.value;
+        if (e.target.innerHTML == '=') {
+            if (string.indexOf('log') > -1) {
+                let indexStart = string.indexOf('log');
+                let indexStartBracket = string.indexOf('(');
+                let indexEndBracket = string.indexOf(')');
+                let LogSubstring = string.substring(indexStart, indexEndBracket+1);
+                let FnSubstring = string.substring(indexStartBracket+1, indexEndBracket);
+
+                string = string.replace(LogSubstring, Math.log10(FnSubstring));
+            }
+            else if (string.indexOf('ln') > -1) {
+                let indexStart = string.indexOf('ln');
+                let indexStartBracket = string.indexOf('(');
+                let indexEndBracket = string.indexOf(')');
+                let LogSubstring = string.substring(indexStart, indexEndBracket+1);
+                let FnSubstring = string.substring(indexStartBracket+1, indexEndBracket);
+                
+                string = string.replace(LogSubstring, Math.log(FnSubstring));
+            }
+            string = eval(string);
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = '';
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        else if (e.target.innerHTML == '%') {
+            string = (string) / 100;
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'log') {
+            string = string + e.target.innerHTML + '(';
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'ln') {
+            string = string + e.target.innerHTML + '(';
+            input.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+});
